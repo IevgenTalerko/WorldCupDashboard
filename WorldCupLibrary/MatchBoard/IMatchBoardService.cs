f67309f063fab7aa410abf0696bcd9c6d4101347
Ievgen Talerko <italerko@idiq.com>
2023-12-21 12:39:14 +0100

    added stub for MatchBoardService

---
+++ WorldCupLibrary/MatchBoard/IMatchBoardService.cs
@@ -0,0 +1,15 @@
+﻿using WorldCupLibrary.Entities;
+using Match = System.Text.RegularExpressions.Match;
+
+namespace WorldCupLibrary.MatchBoard;
+
+public interface IMatchBoardService
+{
+    void StartMatch(int homeTeamId, int awayTeamId);
+
+    void UpdateScore(Guid matchId, int homeTeamGoals, int awayTeamGoals);
+
+    void FinishMatch(Guid matchId);
+
+    List<Match> GetMatchesInProgress();
+}
---
+++ WorldCupLibrary/MatchBoard/MatchBoardService.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+
+namespace WorldCupLibrary.MatchBoard;
+
+public class MatchBoardService : IMatchBoardService
+{
+    public void StartMatch(int homeTeamId, int awayTeamId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateScore(Guid matchId, int homeTeamGoals, int awayTeamGoals)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void FinishMatch(Guid matchId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Match> GetMatchesInProgress()
+    {
+        throw new NotImplementedException();
+    }
+}
