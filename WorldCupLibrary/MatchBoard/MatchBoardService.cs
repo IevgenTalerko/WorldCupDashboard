71f788520134bdf206acf35c483175fc90e73ac0
Ievgen Talerko <italerko@idiq.com>
2023-12-21 17:19:25 +0100

    added validations for score update and match existing

--- WorldCupLibrary/MatchBoard/MatchBoardService.cs
+++ WorldCupLibrary/MatchBoard/MatchBoardService.cs
@@ -28,6 +28,8 @@ public class MatchBoardService : IMatchBoardService
     public void UpdateScore(Guid matchId, int homeTeamGoals, int awayTeamGoals)
     {
         var matchToUpdate = LocalStorage.Matches.SingleOrDefault(x => x.Id == matchId);
+        ValidateUpdateScore(matchToUpdate, homeTeamGoals, awayTeamGoals);
+        
         matchToUpdate.HomeTeamGoals = homeTeamGoals;
         matchToUpdate.AwayTeamGoals = awayTeamGoals;
     }
@@ -35,6 +37,8 @@ public class MatchBoardService : IMatchBoardService
     public void FinishMatch(Guid matchId)
     {
         var matchToFinish = LocalStorage.Matches.SingleOrDefault(x => x.Id == matchId);
+        ValidateMatchExists(matchToFinish);
+        
         matchToFinish.Status = MatchStatus.Finished;
     }
 
@@ -57,5 +61,27 @@ public class MatchBoardService : IMatchBoardService
     {
         return LocalStorage.Matches.Any(x =>
             (x.HomeTeamId == teamId || x.AwayTeamId == teamId) && x.Status == MatchStatus.Started);
+    }
+    
+    private void ValidateUpdateScore(Match? match, int homeTeamGoals, int awayTeamGoals)
+    {
+        ValidateMatchExists(match);
+
+        if (match.Status != MatchStatus.Started)
+            throw new InvalidOperationException("Only matches with Started status can be updated");
+
+        if (Math.Abs(match.HomeTeamGoals - homeTeamGoals) > 1 ||
+            Math.Abs(match.AwayTeamGoals - awayTeamGoals) > 1)
+            throw new InvalidOperationException("Can't add more than one goal in one operation");
+        
+        if (match.HomeTeamGoals != homeTeamGoals &&
+            match.AwayTeamGoals != awayTeamGoals)
+            throw new InvalidOperationException("Can't update both teams score in one operation");
+    }
+
+    private void ValidateMatchExists(Match? match)
+    {
+        if (match is null)
+            throw new ArgumentException("Match wasn't found");
     }
 }
